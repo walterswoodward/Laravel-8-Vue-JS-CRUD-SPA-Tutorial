27ac588ef05ab753662ed103920fc18e6ea42681
walterswoodward <walter.sekope.woodward@gmail.com>
2021-02-19 15:09:18 -0800

    Define CRUD api routes

--- routes/api.php
+++ routes/api.php
@@ -15,6 +15,10 @@ use App\Http\Controllers\ProductController;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::middleware('api')->group(function () {
+    Route::resource('products', ProductController::class);
 });
